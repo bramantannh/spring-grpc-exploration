f3e21922a754e732c9524c2f458bf29ffe4dc296
bramantannh <bramanta.n.n.haq@gdplabs.id>
2022-07-04 21:22:08 +0700

    update rest server impl

--- rest-server/src/main/java/com/glx/exploration/restserver/controller/RestServerController.java
+++ rest-server/src/main/java/com/glx/exploration/restserver/controller/RestServerController.java
@@ -1,10 +1,9 @@
 package com.glx.exploration.restserver.controller;
 
-import com.glx.exploration.restserver.model.input.CompoundInterestModel;
-import com.glx.exploration.restserver.model.input.TextModel;
+import com.glx.exploration.restserver.model.input.CompoundInterestInput;
+import com.glx.exploration.restserver.model.input.TextInput;
 import com.glx.exploration.restserver.service.RestServerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,13 +13,13 @@ public class RestServerController {
 
   @Autowired private RestServerService restServerService;
 
-  @GetMapping("/compoundInterest")
-  public Double powerOf(@RequestBody CompoundInterestModel input) {
+  @PostMapping("/compoundInterest")
+  public Double powerOf(@RequestBody CompoundInterestInput input) {
     return restServerService.calculateFinalAmount(input);
   }
 
   @PostMapping("/saveText")
-  public boolean saveText(@RequestBody TextModel input) {
+  public boolean saveText(@RequestBody TextInput input) {
     return restServerService.saveText(input);
   }
 }
--- rest-server/src/main/java/com/glx/exploration/restserver/service/RestServerService.java
+++ rest-server/src/main/java/com/glx/exploration/restserver/service/RestServerService.java
@@ -1,8 +1,8 @@
 package com.glx.exploration.restserver.service;
 
 import com.glx.exploration.restserver.model.Text;
-import com.glx.exploration.restserver.model.input.CompoundInterestModel;
-import com.glx.exploration.restserver.model.input.TextModel;
+import com.glx.exploration.restserver.model.input.CompoundInterestInput;
+import com.glx.exploration.restserver.model.input.TextInput;
 import com.glx.exploration.restserver.repository.TextRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,15 +12,15 @@ public class RestServerService {
 
   @Autowired private TextRepository textRepository;
 
-  public Double calculateFinalAmount(CompoundInterestModel input) {
-    int compoundInterval = 12;
+  public Double calculateFinalAmount(CompoundInterestInput input) {
+    double compoundInterval = 12.0;
 
     return input.getInitialBalance()
         * Math.pow(1 + input.getInterestRate() / compoundInterval,
         compoundInterval * input.getYearElapsed());
   }
 
-  public boolean saveText(TextModel input) {
+  public boolean saveText(TextInput input) {
     Text text = Text.builder()
         .content(input.getContent())
         .build();
